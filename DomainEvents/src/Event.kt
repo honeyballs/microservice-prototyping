09e63701614acd6981247309a375f4dbcfdb38d6
Yannick Bals <yannick.bals@web.de>
2019-11-15 13:51:23 +0100

    remove unused project

--- DomainEvents/src/Event.kt
+++
@@ -1,9 +0,0 @@
-/**
- * Defines the Event properties that every event needs which are an id and a timestamp.
- */
-interface Event {
-
-    val originatingServiceName: String
-    val id: String
-    val eventCreatedAt: String
-}
